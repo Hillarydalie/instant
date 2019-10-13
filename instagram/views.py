21b9aa8d62ddc302f5a0d50e5f43f2a8bdcc7c9e
hillarydalie <hidalie@gmail.com>
2019-10-14 00:08:49 +0300

    Create a view file for displaying our functionality on our templates.

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate,login,logout
+from django.http import HttpResponse,Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render,redirect
+from django.urls import reverse
+# from .forms import SignUpForm
+import datetime as dt
+# from .models import Image
 
-# Create your views here.
+# Create your home page views here.
+def home(request):
+    # images = Image.get_images()
+    return render(request, 'index.html')
