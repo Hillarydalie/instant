1756dbfcf4f93ddf3fc35b0c82012407bd5a1c64
hillarydalie <hidalie@gmail.com>
2019-10-16 21:49:06 +0300

    Create Image upload form for the model class.

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.views.generic.edit import CreateView, UpdateView, DeleteView, ListView
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView
+from .models import Image, Comment, Profile
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Email Required!')
@@ -10,4 +11,25 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'password1', 'password2', )
 
+class CreateComment(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['comment']
+
+class ImageUpload(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ['image','image_name','image_caption']
+
 # class ImageUpload(CreateView):
+
+#     class Meta:
+#         model = Image
+#         fields = ['profile', 'user_profile', 'likes']
+#         exclude = ['user']
+
+# class UpdateProfile(forms.ModelForm):
+#     class Meta:
+#         model = Profile
+#         fields = ['bio', 'profile_picture']
+
