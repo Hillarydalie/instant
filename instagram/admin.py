0b1ebe84619c37580e1a30f5c9e0be6638cca718
hillarydalie <hidalie@gmail.com>
2019-10-16 21:47:58 +0300

    Register Image,Comment to our admin register.

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Profile,Tag,Location
+from .models import Profile, Image, Comment
 
 # Register your models here.
 admin.site.register(Profile)
-admin.site.register(Tag)
-admin.site.register(Location)
+admin.site.register(Image)
+admin.site.register(Comment)
