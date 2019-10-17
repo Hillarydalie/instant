ab484c7cd66fcc8b122f2645521a00c611ca20c4
hillarydalie <hidalie@gmail.com>
2019-10-17 08:47:26 +0300

    Update url for the project and various functions.

--- instagram/urls.py
+++ instagram/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import url,include
 from . import views as main_views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path,re_path
 from django.contrib.auth import views as auth_views
 from django.contrib.auth import authenticate, login
+# from .views import ImageCreateView
 
 # Template URLS
 app_name = 'instagram'
@@ -12,8 +13,10 @@ app_name = 'instagram'
 urlpatterns = [
     path('', main_views.index , name = 'index'),
     path('signup/', main_views.signup, name='signup'),
-    path('login/',auth_views.LoginView.as_view(template_name = 'login.html'),name='login'),
-    path('logout/',auth_views.LoginView.as_view(template_name = 'logout.html'),name='logout'),
+    path('post/new/', main_views.uploadimage, name = "newpost"),
+    path('profile/', main_views.profile, name="profile"),
+    path('update/',main_views.update_profile,name='update'),
+    re_path(r'^search/$',main_views.searchprofile,name='search'),
 ]
 
 
--- instant/urls.py
+++ instant/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url,include
+from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/',auth_views.LoginView.as_view(template_name = 'login.html'),name='login'),
+    url(r'^logout/',auth_views.LogoutView.as_view(template_name = 'logout.html'),name='logout'),
     url(r'', include('instagram.urls')),
 ]
